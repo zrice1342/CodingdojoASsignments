87c157b21ab1ce895f6522d6c3ea6d6434b37d6a
zrice1342 <zrice1342@gmail.com>
2021-07-09 16:40:43 -0400

    hi

--- Python/flask/Checkerboard/server.py
+++ Python/flask/Checkerboard/server.py
@@ -2,10 +2,12 @@ from flask import Flask, render_template  # Import Flask to allow us to create o
 app = Flask(__name__)    # Create a new instance of the Flask class called "app"
 	# notice the 2 new named argume
 
-@app.route("/play",defaults = {'x':3})
-@app.route("/play/<int:x>",defaults = {'color': "blue"})
-@app.route("/play/<int:x>/<color>")
-def index(x,color):
-    return render_template("index.html", times = x,c = color)
+@app.route("/",defaults = {'x':8,'y':8})
+@app.route("/<int:x>",defaults = {'y':8})
+@app.route("/<int:x>/<int:y>")
+def index(x,y):
+    return render_template("index.html",x = int(y/2),y = int(x/2))
+
+
 if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    THIS SHOULD BE LAST THING IN SERVER FILE
     app.run(debug=True)    # Run the app in debug mode.
