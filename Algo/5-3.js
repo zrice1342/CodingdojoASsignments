e763889c766b8617f55a76d99d83f1179990bd4a
zrice1342 <zrice1342@gmail.com>
2021-08-04 11:18:50 -0400

    Hw

---
+++ Algo/5-3.js
@@ -0,0 +1,14 @@
+// GIVEN AN ARRAY, RETURN ARRAY WITHOUT DUPLICATES
+// [1,2,1,3,4,2] => [1,2,3,4]
+
+// CHALLENGE RETURN THE SAME ARRAY
+
+function removeDupe(arr) {
+arr = arr 
+temparr = []
+for(var i = 0; i < arr.length; i++ );
+    temparr[i] = arr[i]
+}
+
+console.log(removeDupe([1,2,1,3,4,2]))// output == [1,2,3,4]
+console.log(removeDupe([1,2,3]))//output == [1,2,3]
